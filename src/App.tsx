c528b4574a965cad4eb4f58684cf95af195daac5
Frederico Velho <fredericovcoelho@gmail.com>
2021-03-20 21:47:14 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import Modal from 'react-modal';
+
+import { GlobalStyle } from './styles/global';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTrasactionModal';
+import { TransactionsProvider } from '../src/hooks/useTransactions';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+
+    </ TransactionsProvider>
+  );
+}
